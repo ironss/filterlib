b4d69c84672760948e866416ea59746d9524345a
Stephen Irons <ironss@users.noreply.github.com>
2017-10-07 23:52:28 +1300

    All tests pass using Check test framework.

--- test/filter_test.c
+++ test/filter_test.c
@@ -21,8 +21,8 @@
 //	UNUSED(argc);
 //	UNUSED(argv);
 
-    filter_sample_t samples[] =
-    {
+	const filter_sample_t samples[] =
+	{
 //          0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
 //         10,   0,   0,   0,   0,   0,   0,   0,   0,   0,
 //          0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
@@ -32,49 +32,50 @@
 //        102, 101, 100, 101, 120, 100, 101, 102, 101, 100,
 //          0,   5,   0,   0,   0,  -15,   0,   0,   0,   3,
 //          0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-          0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-        100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
-        100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
-          0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-          0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    };
-    int samples_length = sizeof(samples) / sizeof(samples[0]);
+		  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+		100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+		100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+		  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+		  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+	};
+	int samples_length = sizeof(samples) / sizeof(samples[0]);
 
-#if TEST_FILTER_MA
+
+    START_TEST(test_ma)
     {
         #define ma1_LENGTH 5
-        filter_sample_t ma1_x[ma1_LENGTH];
+        filter_sample_t ma1_x[ma1_LENGTH] = {};
         filter_ma_t     ma1;
         filter_ma_init(&ma1, "ma1", ma1_x, ma1_LENGTH);
-        
+
         int i;
         for (i = 0; i < samples_length; i++)
         {
             filter_ma_update(&ma1, samples[i]);
-            
+
             filter_ma_t * f = &ma1;
             printf("%s: %d\n", f->name, f->output);
         }
     }
-#endif
+    END_TEST
 
-#if TEST_FILTER_SPLPF
+    START_TEST(test_splpf)
     {
         filter_splpf_t splpf1;
         filter_splpf_init(&splpf1, "splpf1", 500, 1000);
-        
+
         int i;
         for (i = 0; i < samples_length; i++)
         {
             filter_splpf_update(&splpf1, samples[i]);
-            
+
             filter_splpf_t * f = &splpf1;
             printf("%s: %d\n", f->name, f->output);
         }
     }
-#endif
+    END_TEST
 
-#if TEST_FILTER_HWDEXP
+    START_TEST(test_hwdexp)
     {
         filter_hwdexp_t hwdexp1;
         filter_hwdexp_init(&hwdexp1, "hwdexp1", 500, 0, 10000);
@@ -83,18 +84,18 @@
         for (i = 0; i < samples_length; i++)
         {
             filter_hwdexp_update(&hwdexp1, samples[i]);
-            
+
             filter_hwdexp_t * f = &hwdexp1;
             printf("%s: %d\n", f->name, f->y_t);
         }
     }
-#endif
+    END_TEST
 
     START_TEST(test_iir)
     {
-        #define iir1_LENGTH 5
-        filter_sample_t iir1_x[iir1_LENGTH];
-        filter_sample_t iir1_y[iir1_LENGTH];
+    	#define iir1_LENGTH 5
+        filter_sample_t iir1_x[iir1_LENGTH] = {};
+        filter_sample_t iir1_y[iir1_LENGTH] = {};
         filter_iir_t    iir1;
         filter_iir_init(&iir1, "iir1", iir1_x, iir1_y, filter_iir_cheby_4p04_a, filter_iir_cheby_4p04_b, 10000, iir1_LENGTH);
         
@@ -117,11 +118,14 @@ Suite * iir_suite(void)
     Suite *s;
     TCase *tc_core;
 
-    s = suite_create("test-iir");
+    s = suite_create("test-filters");
 
     /* Core test case */
     tc_core = tcase_create("Core");
 
+    tcase_add_test(tc_core, test_ma);
+    tcase_add_test(tc_core, test_splpf);
+    tcase_add_test(tc_core, test_hwdexp);
     tcase_add_test(tc_core, test_iir);
     suite_add_tcase(s, tc_core);
 
@@ -137,6 +141,7 @@ int main(void)
     s = iir_suite();
     sr = srunner_create(s);
 
+    srunner_set_fork_status(sr, CK_NOFORK);
     srunner_run_all(sr, CK_NORMAL);
     number_failed = srunner_ntests_failed(sr);
     srunner_free(sr);
