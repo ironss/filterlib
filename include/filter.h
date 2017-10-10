a808f4fa06b3289f1aba904ff69180d11f1579b8
Stephen Irons <ironss@users.noreply.github.com>
2017-10-10 15:46:34 +1300

    Added pointers to various sources for the filter code.

--- include/filter.h
+++ include/filter.h
@@ -1,9 +1,16 @@
 // filter.h
 
+// Various filter type
+// See http://www.dspguide.com/pdfbook.htm for a discussion of general DSP principles and
+
 #include <stdint.h>
 
 typedef int16_t filter_sample_t;
 typedef int64_t filter_intermediate_t;
+
+
+// Moving average filter
+// See http://www.dspguide.com/ch15.htm
 
 typedef struct filter_ma {
     const char * name;
@@ -19,6 +26,9 @@ void filter_ma_init(filter_ma_t * filter, const char * name, filter_sample_t * x
 void filter_ma_update(filter_ma_t * filter, filter_sample_t x_n);
 
 
+// Single-pole low-pass filter
+// See http://www.dspguide.com/ch19.htm
+
 typedef struct filter_splpf {
     const char * name;
     filter_sample_t output;
@@ -32,6 +42,9 @@ typedef struct filter_splpf {
 void filter_splpf_init(filter_splpf_t * filter, const char * name, filter_sample_t alpha, filter_sample_t scale);
 void filter_splpf_update(filter_splpf_t * filter, filter_sample_t x_n);
 
+
+// Holt-Winters dual-exponential smoothing
+// See https://en.wikipedia.org/wiki/Exponential_smoothing
 
 typedef struct filter_hwdexp_smoothing {
     const char * name;
@@ -51,6 +64,9 @@ void filter_hwdexp_init(filter_hwdexp_t * filter, const char * name, filter_samp
 void filter_hwdexp_update(filter_hwdexp_t * filter, filter_sample_t x_n);
 
 
+// Generic Infinite impulse response filter
+// See http://www.dspguide.com/ch19.htm
+
 typedef struct filter_iir {
     const char * name;
     filter_sample_t output;
@@ -68,6 +84,10 @@ void filter_iir_init(filter_iir_t * flt, const char * name, filter_sample_t * x,
 void filter_iir_update(filter_iir_t * flt, filter_sample_t x_n);
 
 
+// Coefficients for various different types of IIR filters
+// 4-pole Chebyshev filters at various cutoff frequencies
+// See http://www.dspguide.com/ch20
+
 extern const filter_sample_t filter_iir_cheby_4p01_a[];
 extern const filter_sample_t filter_iir_cheby_4p01_b[];
 extern const filter_sample_t filter_iir_cheby_4p02_a[];
@@ -76,4 +96,3 @@ extern const filter_sample_t filter_iir_cheby_4p03_a[];
 extern const filter_sample_t filter_iir_cheby_4p03_b[];
 extern const filter_sample_t filter_iir_cheby_4p04_a[];
 extern const filter_sample_t filter_iir_cheby_4p04_b[];
-
